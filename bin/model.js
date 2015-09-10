00da323e151424fcdabe0ac8d1d37e389c0cc3cb
jiumogaoao <394127821@qq.com>
2015-09-11 07:26:05 +0800

    change
    change

--- bin/model.js
+++ bin/model.js
@@ -8,14 +8,58 @@
 				fn(idArry[id]);
 				}else{
 				idArry[id]=	new modelArry[name].fn(target);
-				if((modelArry[name].html.length||modelArry[name].css.length)&&(!(modelArry[name].htmlArry.length||modelArry[name].cssArry.length))){
-					var count=0;
-				$.each(modelArry[name],function(i,n){
-					
+				if((modelArry[name].html.length||modelArry[name].css.length)&&(!(modelArry[name].htmlArry.length!==modelArry[name].html.length||modelArry[name].cssArry.length!==modelArry[name].css.length))){
+					var totalUrl=0;
+				function finish(){
+					if(totalUrl === modelArry[name].css.length+modelArry[name].html.length){
+								idArry[id].html=modelArry[name].htmlArry;
+								idArry[id].css=modelArry[name].cssArry;
+								idArry[id].hide=function(){$(target).find("#"+id).hide()};
+								idArry[id].show=function(){$(target).find("#"+id).show()};
+								idArry[id].remove=function(){
+									$(target).find("#"+id).remove();
+									delete idArry[id];
+									};
+								fn(idArry[id]);
+								}
+					};
+				$.each(modelArry[name].css,function(i,n){	
+						var urlNum=i;
+						config.loadingOn();
+						$.ajax({ 
+							url:"css/"+n+".css",
+							dataType:"text",
+							cache:true,
+							error:function(err){
+								config.loadingOff();
+								alert("错误"+JSON.stringify(err));
+								},
+							success: function(data){							
+							modelArry[name].cssArry[urlNum]=data;
+							totalUrl++;
+							finish();
+							}
+						});
+					});
+				$.each(modelArry[name].html,function(i,n){	
+						var urlNum=i;
+						config.loadingOn();
+						$.ajax({ 
+							url:"view/"+n+".html",
+							dataType:"html",
+							cache:true,
+							error:function(err){
+								config.loadingOff();
+								alert("错误"+JSON.stringify(err));
+								},
+							success: function(data){							
+							modelArry[name].htmlArry[urlNum]=data;
+							totalUrl++;
+							finish();
+							}
+						});
 					});
 					}
-				
-				
 				fn(idArry[id]);
 					}
 			}
@@ -42,29 +86,5 @@
 	function set(data){
 		modelArry[data.name]=data;
 		};
-	function changePage(){
-		var hash="index";
-		if(location.hash){
-			hash=location.hash.replace("#","");
-			}
-		var hashArry=hash.split("/");
-		function runRoute(){	
-				var dataObj={};
-				if(routeArry[hashArry[0]].par){
-					var dataArry=routeArry[hashArry[0]].par.split("/");
-					for(var i=0;i<dataArry.length;i++){
-				dataObj[dataArry[i]]=hashArry[i+1];
-				}
-					}
-				routeArry[hashArry[0]].fn(dataObj);	
-			}
-		if(routeArry[hashArry[0]]){
-			runRoute();
-			}else{
-				config.loadingOn();
-				
-			
-			}
-		}
-	
+		
 	})($,app.model,config);
